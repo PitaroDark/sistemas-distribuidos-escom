a6a15212096ca9c1264f5ac7c165f8a6b3f23a7e
PitaroDark <dark.2002ikari@gmail.com>
2024-10-29 11:21:38 -0600

    Refactor: Upgrading clase 24

--- clase24/HttpClientSynchronous.java
+++ clase24/HttpClientSynchronous.java
@@ -10,7 +10,9 @@ import java.time.Duration;
 public class HttpClientSynchronous {
   private static final String STATUS_ENDPOINT = "/status";
   private static final String SEARCHTOKEN_ENDPOINT = "/searchtoken";
-  private static final String BUCKET_STRAY_ENRPOINT = "https://storage.googleapis.com/storage/v1/b/first_bucket_brandon/o/stray.jpg";
+  //CAMBIAR POR EL ENDPOINT DE LA IMAGEN DE TU BUCKET
+  private static final String STRAY_IMAGE_ENDPOINT = "https://storage.googleapis.com/storage/v1/b/first_bucket_brandon/o/stray.jpg";
+  //CAMBIAR POR TU TOKEN
   private static final String TOKEN = "ya29.c.c0ASRK0GbiEa05QyPKNWLM37VvQ5ybgQYH8JEvaMTbJ99b5I521PWQdrOvDjti9k9wVsdaKhZjCHaP1w-dbclajG5IuuGpNaG3xod9_B9d26pHFJYEl4L7GOmbEUXo7ZUcxfqYWOt1qfOaAxywFClvjdvjOG9iz7hPy-Xr7JkoPn4iXLAYDuE91ywIWqCgiSU2FhuysH-rWoqO22mNIYUhPBbyVoGzo9uxTa_rMyQOOOE-adWVo700-le0La-D9M5FJ2IvV8GrXA6e5O2YmD9kKI1lj424aJwd0S2iWiQOCIFbCDjlm0RGbu4imYqddfk3fP4pNrOOZduGke3GQQm4UQWshRq_9dYXC3YwIxB_T9Jv93P5iElsH13lk7sMOfTHhEz6OQL399KJYr1rqM9IpFeOfgiigOIffng9bOv8px8Zm30xrmyOZ88vzozejJnxtRUQt5-l4zwsJea3oJ-h8SouXxwz77JcFzr4skbaxy0bQ6dtS_SmyF9h9zydJvRlV2mw2vk9ZifWrkn4QUJvbF6VXh6-MyaWb5Z0_u1kX9VBRzjukrQB2cajgM99j3j6-obygfbrewnzjbSRm56WgIwg1Rt3BXR5pR1Ms69lYx9avRk1m06-uFescs2cauMjQ608Ilec-ZVd6Zfdrm4lUuiFMWvVZ27I6kpe8Rqfstx97OwuM57v8Fomcujuub-uq3cBlOStSntFFVd8wM89uk19iOXm4g-Q_u255e3UivcIhIm91a7Ozpbb_i4Sh2YXajX_4o69mVpWjSqB49VuJSZxbl1BQfnJ0qk0YjVJ9-lY8RX09M-I64yxd0zy-6WzdQ1mUyt__reRnk8QX2JthVSV4vS2eia2b3127g78nc0yiizbMjf5a7jkcmrOMYfwOjR4wISeW17xBizlvzS7XvuYif5sS5M89SiluFnQ1vUJ-B5du5Y8kahZegc977ar2sIvz2Xki3i6UO7Ze2Oy0k8eZjhOVb3XcsiV3JjhyQyQIxvczi9z18J";
 
   private static final HttpClient httpClient = HttpClient.newBuilder()
@@ -22,7 +24,7 @@ public class HttpClientSynchronous {
 
     HttpRequest request = HttpRequest.newBuilder()
         .GET()
-        .uri(URI.create(BUCKET_STRAY_ENRPOINT))
+        .uri(URI.create(STRAY_IMAGE_ENDPOINT))
         .setHeader("Authorization", "Bearer " + TOKEN)
         .setHeader("User-Agent", "Java 11 HttpClient Bot") // add request header
         .setHeader("X-Debug", "true")
